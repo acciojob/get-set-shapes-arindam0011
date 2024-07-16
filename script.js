830056c668bb3c7da8645da80e3a547b9c7b1f7a
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-17 01:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width,
+		this.height=height
+	}
+	getArea(){ return this.width*this.height};
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		super(side,side);
+	}
+	getPerimeter(){return this.width*4};
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
